eeb4ee3a28d1986123bc86640e06409503746fc8
Rustam <rust66637@gmail.com>
2020-02-11 00:29:06 +0300

    df

--- hashtable.py
+++ hashtable.py
@@ -5,10 +5,11 @@ class HashTable:
         self.slots = [None] * self.size
 
      def hash_fun(self, value):
-         # в качестве value поступают строки!
-
-         # всегда возвращает корректный индекс слота
-         return 0
+         p = 0.33
+         summ = 0
+         for i in value:
+             summ += ord(i) * p
+         return int(summ % self.size)
 
      def seek_slot(self, value):
          # находит индекс пустого слота для значения, или None
@@ -16,12 +17,23 @@ class HashTable:
 
      def put(self, value):
          # записываем значение по хэш-функции
-         
+
          # возвращается индекс слота или None,
          # если из-за коллизий элемент не удаётся
-         # разместить 
+         # разместить
          return None
 
      def find(self, value):
          # находит индекс слота со значением, или None
          return None
+'''
+    def seek_slot(self, value):
+        None in self.slots:
+            index = hash_fun(value)
+            while
+            if self.slots[index] == None:
+                return index
+        else:
+            # находит индекс пустого слота для значения, или None
+            return None
+'''
