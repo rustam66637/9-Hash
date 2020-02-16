6445425fddcb7022b2279cb0e7e40a51989db822
Rustam <rust66637@gmail.com>
2020-02-16 15:28:40 +0300

    upd all

--- hashtable.py
+++ hashtable.py
@@ -4,36 +4,42 @@ class HashTable:
         self.step = stp
         self.slots = [None] * self.size
 
-     def hash_fun(self, value):
-         p = 0.33
-         summ = 0
-         for i in value:
-             summ += ord(i) * p
-         return int(summ % self.size)
+    def hash_fun(self, value):
+        p = 0.33
+        summ = 0
 
-     def seek_slot(self, value):
-         # находит индекс пустого слота для значения, или None
-         return None
+        for i in value:
+            summ += ord(i) * p
+        return int(summ % self.size)
 
-     def put(self, value):
-         # записываем значение по хэш-функции
-
-         # возвращается индекс слота или None,
-         # если из-за коллизий элемент не удаётся
-         # разместить
-         return None
-
-     def find(self, value):
-         # находит индекс слота со значением, или None
-         return None
-'''
     def seek_slot(self, value):
-        None in self.slots:
-            index = hash_fun(value)
-            while
-            if self.slots[index] == None:
-                return index
+        if None in self.slots:
+            i = self.hash_fun(value)
+
+            while True:
+                try:
+                    if slots[i] is None:
+                        return i
+                    else:
+                        i += step
+                except IndexError:
+                    i = 0
+                    step //= 2
         else:
-            # находит индекс пустого слота для значения, или None
             return None
-'''
+
+    def put(self, value):
+        valueIndex = self.hash_fun(value)
+
+        if self.slots[valueIndex] is None:
+            self.slots[valueIndex] = value # записываем значение по хэш-функции
+        elif None in self.slots:
+            slots[self.seek_slot(valueIndex)] = value # возвращается индекс слота
+        else:
+            return None
+
+    def find(self, value): # находит индекс слота со значением, или None
+        try:
+            return self.slots.index(value)
+        except ValueError:
+            return None
